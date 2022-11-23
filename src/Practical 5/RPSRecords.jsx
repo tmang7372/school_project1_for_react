c3e6a9218affa30c81f4033643a115cf08d59a77
angtm3907 <116273667+angtm3907@users.noreply.github.com>
2022-11-23 17:19:23 +0800

    Practical 6

--- src/Practical 5/RPSRecords.jsx
+++ src/Practical 5/RPSRecords.jsx
@@ -1,7 +1,23 @@
 export default function RPSRecords(props) {
+	let total = 0;
+    let percent = 0;
+    if(props.records.length == 0 ){
+        props.percentage = 0;
+    }else {
+       alert("inside if loop")
+		for(let i=0;i<props.records.length;i++){
+        console.log("inside for loop")  
+		if(props.records[i].result=="Win"){
+	         alert("inside if");
+	         total = total+1;
+    }
+        percent = (total/props.records.length*100)
+        
+    }} 
+	
   return (
     <div>
-      <p>Rounds:</p>
+      <p>Rounds:<span>Win % {percent.toFixed(2)}%</span></p>
       <ol>
         {props.records.map(function (record) {
            return <li>
